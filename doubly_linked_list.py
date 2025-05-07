d2acca3bfe7c9831f0e83edf35db22269f00746f
sir-3e0 <ewimua@outlook.com>
2025-05-07 18:05:43 -0400

    Added a few files for DSA

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -2,9 +2,10 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+        self.prev = None
         
 
-class LinkedList:
+class DoublyLinkedList:
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
@@ -19,28 +20,28 @@ class LinkedList:
         
     def append(self, value):
         new_node = Node(value)
-        if self.length == 0:
+        if self.head is None:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
+            new_node.prev = self.tail
             self.tail = new_node
         self.length += 1
         return True
+    
     def pop(self):
         if self.length == 0:
             return None
-        temp = self.head
-        pre = self.head
-        while(temp.next):
-            pre = temp
-            temp = temp.next
-        self.tail = pre
-        self.tail.next = None
-        self.length -= 1
-        if self.length == 0:
+        temp = self.tail
+        if self.length == 1:
             self.head = None
-            self.tail = None
+            self.tail = None 
+        else:       
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
         return temp
 
     def prepend(self, value):
@@ -50,28 +51,38 @@ class LinkedList:
             self.tail = new_node
         else:
             new_node.next = self.head
+            self.head.prev = new_node
             self.head = new_node
         self.length += 1
         return True
-
+    
     def pop_first(self):
         if self.length == 0:
             return None
         temp = self.head
-        self.head = self.head.next
-        temp.next = None
-        self.length -= 1
-        if self.length == 0:
+        if self.length == 1:
+            self.head = None
             self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None      
+        self.length -= 1
         return temp
+
     def get(self, index):
         if index < 0 or index >= self.length:
             return None
         temp = self.head
-        for _ in range(index):
-            temp = temp.next
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev  
         return temp
-        
+    
     def set_value(self, index, value):
         temp = self.get(index)
         if temp:
@@ -86,65 +97,83 @@ class LinkedList:
             return self.prepend(value)
         if index == self.length:
             return self.append(value)
+        
         new_node = Node(value)
-        temp = self.get(index - 1)
-        new_node.next = temp.next
-        temp.next = new_node
+        before = self.get(index - 1)
+        after = before.next
+
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+        
         self.length += 1   
         return True  
+    
     def remove(self, index):
         if index < 0 or index >= self.length:
             return None
         if index == 0:
             return self.pop_first()
-        if index == self.length - 1:
+        if index is self.length - 1:
             return self.pop()
-        pre = self.get(index - 1)
-        temp = pre.next
-        pre.next = temp.next
+
+        temp = self.get(index)
+        
+        temp.next.prev = temp.prev
+        temp.prev.next = temp.next
         temp.next = None
+        temp.prev = None
+
         self.length -= 1
         return temp
+    
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
+my_doubly_linked_list.append(3)
+my_doubly_linked_list.append(4)
+my_doubly_linked_list.append(5)
+my_doubly_linked_list.print_list()
 
-    def reverse(self):
-        temp = self.head
-        self.head = self.tail
-        self.tail = temp
-        after = temp.next
-        before = None
-        for _ in range(self.length):
-            after = temp.next
-            temp.next = before
-            before = temp
-            temp = after
+print('\nRemoved node:')
+print(my_doubly_linked_list.remove(0).value)
+print('DLL after remove() of first node:')
+my_doubly_linked_list.print_list()
 
-
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(4)
-
-print('LL before reverse():')
-my_linked_list.print_list()
-
-my_linked_list.reverse()
-
-print('\nLL after reverse():')
-my_linked_list.print_list()
+print('\nRemoved node:')
+print(my_doubly_linked_list.remove(2).value)
+print('DLL after remove() of last node:')
+my_doubly_linked_list.print_list()
 
 """
     EXPECTED OUTPUT:
     ----------------
-    LL before reverse():
+    DLL before remove():
     1
     2
     3
+    4
+    5
+
+    Removed node:
+    3
+    DLL after remove() in middle:
+    1
+    2
+    4
+    5
+
+    Removed node:
+    1
+    DLL after remove() of first node:
+    2
+    4
+    5
+
+    Removed node:
+    5
+    DLL after remove() of last node:
+    2
     4
 
-    LL after reverse():
-    4
-    3
-    2
-    1
-    
 """
